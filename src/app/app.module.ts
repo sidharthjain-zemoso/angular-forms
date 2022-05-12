59d063ee6170ae00784c4b8b041a49177eec0c91
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-12 08:26:02 +0530

    creating and syncing the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-
+//forms module is required for template driven
+//reactive forms module for reactive approach
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
